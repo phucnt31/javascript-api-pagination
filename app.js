948efb7dfec07fe9ec36728a3a77c59f49a29375
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-29 18:55:20 +0700

    Fetch followers

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+import fetchFollowers from "./fetchFollowers.js";
 
+const init = async () => {
+  const followers = await fetchFollowers();
+  console.log(followers);
+};
+
+window.addEventListener("load", init);
