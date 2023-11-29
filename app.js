492c82a9f3b663e460a6441fbca6a35d7c2a69fa
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-29 19:25:44 +0700

    Display buttons

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 import fetchFollowers from "./fetchFollowers.js";
 import displayFollowers from "./displayFollowers.js";
 import paginate from "./paginate.js";
+import displayButtons from "./displayButtons.js";
 
 const title = document.querySelector(".section-title h1");
+const btnContainer = document.querySelector(".btn-container");
+
+let index = 0;
+let pages = [];
+
+const setupUI = () => {
+  displayFollowers(pages[index]);
+  displayButtons(btnContainer, pages, index);
+};
 
 const init = async () => {
   const followers = await fetchFollowers();
-  displayFollowers(followers);
   title.textContent = "pagination";
-  const pages = paginate(followers);
-  console.log(pages);
+  pages = paginate(followers);
+  setupUI();
 };
 
 window.addEventListener("load", init);
