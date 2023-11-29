b4f9fbc2b00a71ca152e544ab8a9c8b4075bab40
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-29 19:01:05 +0700

    Display followers

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import fetchFollowers from "./fetchFollowers.js";
+import displayFollowers from "./displayFollowers.js";
+
+const title = document.querySelector(".section-title h1");
 
 const init = async () => {
   const followers = await fetchFollowers();
-  console.log(followers);
+  displayFollowers(followers);
+  title.textContent = "pagination";
 };
 
 window.addEventListener("load", init);
